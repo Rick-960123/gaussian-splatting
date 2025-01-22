da1cf81cf751a10f11e4a7717d661bb2040af247
rick <g539983038@gmail.com>
2025-01-22 13:25:10 +0800

    修改可视化

--- visColmapPose.py
+++ visColmapPose.py
@@ -9,32 +9,22 @@ from visualization_msgs.msg import Marker, MarkerArray
 from scipy.spatial.transform import Rotation, Slerp
 import matplotlib.pyplot as plt
 import threading
-import os
 
 class ColmapPoseVisualizer:
-    def __init__(self, node_name, path, plot=False):
+    def __init__(self, node_name:str, paths:list):
         # 创建发布器
-        self.pose_pub = rospy.Publisher(f'/{node_name}/camera_poses', PoseStamped, queue_size=10)
-        self.path_pub = rospy.Publisher(f'/{node_name}/camera_trajectory', Path, queue_size=10)
-        self.marker_pub = rospy.Publisher(f'{node_name}/camera_markers', MarkerArray, queue_size=10)
-        
-        self.path = Path()
-        self.path.header.frame_id = "map"
-        self.marker_array = MarkerArray()
-        self.marker_id = 0
-
         self.scale = 1.0
-        self.plot = plot
+        self.publishers = []
+        self.poses = []
+        for idx, path in enumerate(paths):
+            pose_pub = rospy.Publisher(f'/{node_name}_{idx}/camera_poses', PoseStamped, queue_size=10)
+            path_pub = rospy.Publisher(f'/{node_name}_{idx}/camera_trajectory', Path, queue_size=10)
+            self.publishers.append({pose_pub:pose_pub, path_pub:path_pub})
+            poses = self.read_images_text(path)
+            self.poses.append(poses)
+            threading.Thread(target=self.publish_poses, args=(self.poses[idx], self.publishers[idx][pose_pub], self.publishers[idx][path_pub])).start()
 
-        if node_name == "colmap2":
-            self.scale = 0.741
-
-        poses = self.read_images_text(path)
-        if self.plot:
-            self.publish_poses(poses)
-        else:
-            new_thread = threading.Thread(target=self.publish_poses, args=(poses,))
-            new_thread.start()
+        self.plot_poses(self.poses)
 
     def read_images_text(self, path):
         """读取COLMAP的images.txt文件"""
@@ -78,41 +68,87 @@ class ColmapPoseVisualizer:
             # 跳过下一行（包含特征点信息）
             line_index += 2
 
-            if image_id == 602:
-                break
         print(f"End Pose Image Name: {image_name}")
 
         return poses
+    
+    def plot_poses(self, poseslist):
+        plot_data = []
 
-    def publish_poses(self, poses):
-        x_data, y_data, z_data, qx_data, qy_data, qz_data, qw_data, x_o, y_o, z_o, imageids = [], [], [], [], [], [], [], [], [], [], []
+        for poses in poseslist:
+            x_data, y_data, z_data, qx_data, qy_data, qz_data, qw_data, x_o, y_o, z_o, imageids = [], [], [], [], [], [], [], [], [], [], []
+            for timestamp, pose in sorted(poses.items()):
+                # 创建PoseStamped消息
+                pose_msg = PoseStamped()
+                pose_msg.header.frame_id = "map"
+                pose_msg.header.stamp = rospy.Time.now()
+                
+                # 获取位置和方向
+                position = pose[:3, 3]
+                quaternion = tf.transformations.quaternion_from_matrix(pose)
+                
+                x_data.append(position[0])
+                y_data.append(position[1])
+                z_data.append(position[2])
+                qx_data.append(quaternion[0])
+                qy_data.append(quaternion[1])
+                qz_data.append(quaternion[2])
+                qw_data.append(quaternion[3])
+                imageids.append(timestamp)
+
+                euler_angles = tf.transformations.euler_from_matrix(pose)
+                euler_x, euler_y, euler_z = euler_angles
+                
+                x_o.append(euler_x * 180/3.14159)
+                y_o.append(euler_y* 180/3.14159)
+                z_o.append(euler_z* 180/3.14159)
+
+            plot_data.append({"x_data":x_data, "y_data":y_data, "z_data":z_data, "qx_data":qx_data, "qy_data":qy_data, "qz_data":qz_data, "qw_data":qw_data, "x_o":x_o, "y_o":y_o, "z_o":z_o, "imageids":imageids})
+        
+        fig, ax = plt.subplots()
+        fig1, ax1 = plt.subplots()
+        fig2, ax2 = plt.subplots()
+
+        for idx, data in enumerate(plot_data):
+            ax.plot(data['imageids'], data['x_data'], label=f"Position X {idx}", color='r')
+            ax.plot(data['imageids'], data['y_data'], label=f"Position Y {idx}", color='g')
+            ax.plot(data['imageids'], data['z_data'], label=f"Position Z {idx}", color='b')
+        
+            ax1.plot(data['imageids'], data['qx_data'], label=f"Quaternion X {idx}", color='r')
+            ax1.plot(data['imageids'], data['qy_data'], label=f"Quaternion Y {idx}", color='g')
+            ax1.plot(data['imageids'], data['qz_data'], label=f"Quaternion Z {idx}", color='b')
+            ax1.plot(data['imageids'], data['qw_data'], label=f"Quaternion W {idx}", color='y')
+            
+            ax2.plot(data['imageids'], data['x_o'], label=f"Euler X {idx}", color='r')
+            ax2.plot(data['imageids'], data['y_o'], label=f"Euler Y {idx}", color='g')
+            ax2.plot(data['imageids'], data['z_o'], label=f"Euler Z {idx}", color='b')
+
+        ax.set_xlabel('imageId')
+        ax.set_ylabel('Position')
+        ax1.set_ylabel('Quaternion')
+        ax2.set_ylabel('Euler')
+        ax.set_title('Position Over Time')
+        ax1.set_title('Quaternion Over Time')
+        ax2.set_title('Euler Over Time')
+
+        ax.legend()
+        ax1.legend()
+        ax2.legend()
+        plt.show()
+
+    def publish_poses(self, poses, pose_publisher, path_publisher):
+        path = Path()
+        path.header.frame_id = "map"
 
         for timestamp, pose in sorted(poses.items()):
+            # 获取位置和方向
+            position = pose[:3, 3]
+            quaternion = tf.transformations.quaternion_from_matrix(pose)
+
             # 创建PoseStamped消息
             pose_msg = PoseStamped()
             pose_msg.header.frame_id = "map"
             pose_msg.header.stamp = rospy.Time.now()
-            
-            # 获取位置和方向
-            position = pose[:3, 3]
-            quaternion = tf.transformations.quaternion_from_matrix(pose)
-            
-            x_data.append(position[0])
-            y_data.append(position[1])
-            z_data.append(position[2])
-            qx_data.append(quaternion[0])
-            qy_data.append(quaternion[1])
-            qz_data.append(quaternion[2])
-            qw_data.append(quaternion[3])
-            imageids.append(timestamp)
-
-            euler_angles = tf.transformations.euler_from_matrix(pose)
-            euler_x, euler_y, euler_z = euler_angles
-            
-            x_o.append(euler_x * 180/3.14159)
-            y_o.append(euler_y* 180/3.14159)
-            z_o.append(euler_z* 180/3.14159)
-
             pose_msg.pose.position.x = position[0]
             pose_msg.pose.position.y = position[1]
             pose_msg.pose.position.z = position[2]
@@ -120,41 +156,14 @@ class ColmapPoseVisualizer:
             pose_msg.pose.orientation.y = quaternion[1]
             pose_msg.pose.orientation.z = quaternion[2]
             pose_msg.pose.orientation.w = quaternion[3]
-            
-            # 添加到路径
-            self.path.poses.append(pose_msg)
-            
-            # 发布消息
-            self.pose_pub.publish(pose_msg)
-            self.path_pub.publish(self.path)
-            
-            rospy.sleep(0.01)  # 添加小延迟使可视化更流畅
+            pose_publisher.publish(pose_msg)
 
-        if self.plot:
-            fig, ax = plt.subplots()
-            ax.plot(imageids, x_data, label="Position X", color='r')
-            ax.plot(imageids, y_data, label="Position Y", color='g')
-            ax.plot(imageids, z_data, label="Position Z", color='b')
-            fig1, ax1 = plt.subplots()
-            ax1.plot(imageids, qx_data, label="Quaternion X", color='r')
-            ax1.plot(imageids, qy_data, label="Quaternion Y", color='g')
-            ax1.plot(imageids, qz_data, label="Quaternion Z", color='b')
-            ax1.plot(imageids, qw_data, label="Quaternion W", color='y')
-            fig2, ax2 = plt.subplots()
-            ax2.plot(imageids, x_o, label="Euler X", color='r')
-            ax2.plot(imageids, y_o, label="Euler Y", color='g')
-            ax2.plot(imageids, z_o, label="Euler Z", color='b')
-            ax.set_xlabel('imageId')
-            ax.set_ylabel('Position')
-            ax1.set_ylabel('Quaternion')
-            ax2.set_ylabel('Euler')
-            ax.set_title('Position Over Time')
-            ax1.set_title('Quaternion Over Time')
-            ax2.set_title('Euler Over Time')
-            ax.legend()
-            ax1.legend()
-            ax2.legend()
-            plt.show()
+            # 添加到路径
+            path.header.stamp = rospy.Time.now()
+            path.poses.append(pose_msg)
+            path_publisher.publish(path)
+        
+            rospy.sleep(0.2)  # 添加小延迟使可视化更流畅
 
         while not rospy.is_shutdown():
             pose_msg = PoseStamped()
@@ -172,17 +181,13 @@ class ColmapPoseVisualizer:
             pose_msg.pose.orientation.y = quaternion[1]
             pose_msg.pose.orientation.z = quaternion[2]
             pose_msg.pose.orientation.w = quaternion[3]
-            self.pose_pub.publish(pose_msg)
-            rospy.sleep(0.01)
+            pose_publisher.publish(pose_msg)
+            rospy.sleep(0.2)
 
 def main():
     rospy.init_node("VisColMapPose", anonymous=True)
-
-    colmap_path = "/home/rick/Datasets/slam2000-雪乡情-正走/colmap/sparse/0/images.txt"  # 修改为实际路径
-    colmap_path2 = "/home/rick/Datasets/slam2000-雪乡情-正走/colmap1/sparse/0/images.txt"
-
-    ColmapPoseVisualizer("colmap", colmap_path, True)
-    # ColmapPoseVisualizer("colmap2", colmap_path2, True)
+    colmap_path = ["/home/rick/Datasets/slam2000-雪乡情-正走/colmap/sparse/0/images.txt", "/home/rick/Datasets/slam2000-雪乡情-正走/colmap1/sparse/0/images.txt"]
+    ColmapPoseVisualizer("colmap", colmap_path)
     rospy.spin()
 
 if __name__ == '__main__':
